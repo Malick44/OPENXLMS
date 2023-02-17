bd050fa26b8e6af53389eebc053d4a93e7ef7a4c
Malick Ouedraogo <malickouedraogo@Malicks-iMac.local>
2023-02-17 10:37:27 -0500

    Initial version of OpnLms generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/opnlms/app/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.opnlms.app.cucumber;
+
+import com.opnlms.app.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
---
+++ src/test/java/com/opnlms/app/cucumber/CucumberTestContextConfiguration.java
@@ -0,0 +1,10 @@
+package com.opnlms.app.cucumber;
+
+import com.opnlms.app.IntegrationTest;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@CucumberContextConfiguration
+@IntegrationTest
+@WebAppConfiguration
+public class CucumberTestContextConfiguration {}
